c66c74b8f17ddd996cab11c5119bb14ec4caa9da
Ola Bini <ola.bini@gmail.com>
2009-04-06 12:57:30 +0200

    Update everything to make it ready for public consumption

--- app/controllers/test_controller.rb
+++ app/controllers/test_controller.rb
@@ -1,28 +1,28 @@
 class TestController < ApplicationController
   layout 'blog'
 
-  def index
-    render :text => "Hello world! Current date and time is: #{Time.now}"
-  end
+#   def index
+#     render :text => "Hello world! Current date and time is: #{Time.now}"
+#   end
   
-  def foo
-    @blarg = Time.now
-  end
+#   def foo
+#     @blarg = Time.now
+#   end
   
-  def ruby
-    if request.post?
-      @query = params[:query]
-      begin 
-        @result = Object.new.instance_eval(@query)
-      rescue Exception => e
-        @result = e
-      end
-    end
-  end
+#   def ruby
+#     if request.post?
+#       @query = params[:query]
+#       begin 
+#         @result = Object.new.instance_eval(@query)
+#       rescue Exception => e
+#         @result = e
+#       end
+#     end
+#   end
   
-  def run_tests
-    require 'yak_test'
-    render :text => YAKTest.run_tests(params[:yak])
-    headers['Content-type'] = 'text/plain'
-  end
+#   def run_tests
+#     require 'yak_test'
+#     render :text => YAKTest.run_tests(params[:yak])
+#     headers['Content-type'] = 'text/plain'
+#   end
 end
--- test_script1.rb
+++
@@ -1,9 +0,0 @@
-
-ds = com.google.appengine.api.datastore.DatastoreServiceFactory.datastore_service
-ent = com.google.appengine.api.datastore.Entity.new("foo", "foo42")
-key = ent.get_key
-ent.set_property("hello", "bar") 
-ent.set_property("goodbye", "blarg") 
-ds.put(ent)
-ent2 = ds.get(key)
-ent2.get_properties.to_string
